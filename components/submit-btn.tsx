b52415ccc8e3ab51b31c7b3a76eba52f28d860be
Thomas Sørensen <tsorensen007@gmail.com>
2024-04-07 17:17:17 +0200

    fix: legg til nytt bilde

--- components/submit-btn.tsx
+++ components/submit-btn.tsx
@@ -9,7 +9,7 @@ export default function SubmitBTN() {
   return (
     <button type="submit" className='group flex items-center justify-center h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transtition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 disabled:scale-100 disabled:bg-opacity-65' disabled={pending}> {pending ? (<div className='h-5 w-5 animate-spin rounded-full border-b-2 border-white'></div>) : (
     
-                <>Submit{" "} <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1'/>{" "}</>)}
+                <>Send {" "} <FaPaperPlane className='text-xs opacity-70 ml-1 transition-all group-hover:translate-x-1 group-hover:-translate-y-1'/>{" "}</>)}
                 </button>
   )
   
